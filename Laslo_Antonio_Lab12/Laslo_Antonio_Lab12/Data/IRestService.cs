2c26aafa1344cca94b893562d8659fc946533b04
Antonio Laslo <antoniolaslo@yahoo.com>
2021-01-07 12:24:26 +0200

    Add project files.

---
+++ Laslo_Antonio_Lab12/Laslo_Antonio_Lab12/Data/IRestService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Laslo_Antonio_Lab12.Models;
+
+namespace Laslo_Antonio_Lab12.Data
+{
+
+
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+
+}
